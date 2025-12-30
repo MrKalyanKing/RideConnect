e1d795f6aebbf022dcdcca15bed7b0b5e9d5dea2
MrKalyanKing <badhavathkalyan08@gmail.com>
2025-12-30 12:20:31 +0530

    SignUp components

--- Frontend/rideconnect/src/components/CaptainComp/CaptainLoginSignup/CaptainSignUp.jsx
+++ Frontend/rideconnect/src/components/CaptainComp/CaptainLoginSignup/CaptainSignUp.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import './CaptainSignUp.css'
+import cap_bg from '../../../assets/CaptainLogin.png'
+import { Link } from 'react-router-dom'
 const CaptainSignUp = () => {
   return (
-    <div>CaptainSignUp</div>
+    <div className='cap-signup'>
+      <img src={cap_bg} alt="" />
+
+      <div className='cap-signup-box'>
+        <h1>Captain Portal Registration</h1>
+        {/* <h2>Captain Registration</h2> */}
+        <div className='cap-signup-form' >
+          <form action="">
+            <input type="text" placeholder='Name' />
+            <input type="text" placeholder='Email' />
+            <input type="text" placeholder='Phone Number' />
+            <input type="text" placeholder='Password' />
+            <input type="text" placeholder='Confirm Password' />
+            <button type="submit">Sign Up</button>
+            <div className='signup-test'>
+              <span>Already Captain ? <span className='navigate' > <Link to='/captain/login' >Start Riding</Link>  </span> </span>
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
