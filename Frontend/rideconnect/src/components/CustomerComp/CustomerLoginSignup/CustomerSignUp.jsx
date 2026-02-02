42c2f6ee437b924e8a87d317bceaddd3e59c569c
MrKalyanKing <badhavathkalyan08@gmail.com>
2026-02-02 23:24:39 +0530

    feat: Add initial customer sign-up component with basic form fields and navigation.

--- Frontend/rideconnect/src/components/CustomerComp/CustomerLoginSignup/CustomerSignUp.jsx
+++ Frontend/rideconnect/src/components/CustomerComp/CustomerLoginSignup/CustomerSignUp.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './CustomerSignUp.css'
 import signupInterface from '../../../assets/CustomerInterfacebg.png'
 import RideConnect from '../../../assets/Rc.png'
 import { Link } from 'react-router-dom'
 const CustomerSignUp = () => {
+
+  const [data, setData] = useState({
+    phone: "",
+    email: ""
+  })
+
+  const handleOnChange = ((e) => {
+    const { name, value } = e.target
+    setData((prev) => ({ ...prev, [name]: value }))
+  })
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  }
+  useEffect(() => {
+    console.log(data)
+  })
+
+
+
+
   return (
     <div className='signup' >
       <img src={signupInterface} alt="" />
@@ -12,11 +33,22 @@ const CustomerSignUp = () => {
         <div className='logo'>
           <Link to='/' > <img src={RideConnect} alt="" /></Link>
         </div>
-        <form action="">
+        <form onSubmit={handleOnSubmit} action="">
           <div>
-            <input type="text" name="" id="" placeholder='Phone Number' />
-            <input type="text" name="" id="" placeholder='Email' />
+            <input type="text"
+              name="phone"
+              value={data.phone}
+              onChange={handleOnChange}
+              placeholder='Phone Number' />
+
+
+            <input type="text"
+              name="email"
+              value={data.email}
+              onChange={handleOnChange}
+              placeholder='Email' />
           </div>
+
           <button className='signup-btn' >Start Riding</button>
           <div className='signup-test'>
             <span>Login into RideConnect? <span className='inner-span' ><Link to="/customer/login" >Login</Link></span></span>
